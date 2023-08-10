8cf97f8ec2f24d83c4ee767f525646283f43fd9d
Asiimwe Victor Emmanuel <victor.asiimwe@outlook.com>
2023-08-10 12:58:01 +0300

    member.js complete

---
+++ member.js
@@ -0,0 +1,8 @@
+function skillsMember() {
+  return {
+    restrict: 'E',
+    templateUrl: 'app/components/member/skills-member.html',
+    controller: 'SkillsMemberController',
+    controllerAs: 'skillsMemberCtrl'
+  };
+}
